a8a65b0d279695099f614ee8fac365db1528d26e
Ayush Tomar <ayushsinghtomar184@gmail.com>
2024-12-15 12:30:25 +0530

    update the order summary and payment summary

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 hello();
 const today=(dayjs());
@@ -118,6 +119,7 @@ export function renderOrderSummary(){
         removeFromCart(productId);
         const container=document.querySelector(`.js-cart-item-container-${productId}`)
             container.remove();
+            renderPaymentSummary();
         });
     });
     document.querySelectorAll('.js-delivery-option')
@@ -126,6 +128,7 @@ export function renderOrderSummary(){
             const{productId,deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
