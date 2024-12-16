34a776ec809f8cd543c05ef67d0b9789379b9a72
Ayush Tomar <ayushsinghtomar184@gmail.com>
2024-12-16 13:38:46 +0530

    use jasmine to test our cases and delete extra

--- javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -1,6 +1,6 @@
 import {formatCurrency} from "../scripts/utils/money.js";
 
-console.log('test suite:formateCurrency');
+console.log('test suite:formatCurrency');
 console.log('convert cents into dollars');
  if (formatCurrency(2095)==='20.95'){
     console.log("passed");
